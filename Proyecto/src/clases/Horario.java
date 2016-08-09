85e53ae6481706f24df87fc5e854ee8f04206c54
Nico <nikobazan@hotmail.com>
2016-08-09 15:09:36 -0300

    nuevas clases

---
+++ Proyecto/src/clases/Horario.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author nico
+ */
+public class Horario {
+    String dia;
+    int horaInicio;
+    int horaFin;
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public int getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(int horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public int getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(int horaFin) {
+        this.horaFin = horaFin;
+    }
+        
+}
